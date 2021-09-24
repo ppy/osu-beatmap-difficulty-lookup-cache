760d5d54f64ff7d4f9202c23b9fb555164883054
smoogipoo <smoogipoo@smgi.me>
2021-09-24 13:50:49 +0900

    Add json property names

--- BeatmapDifficultyLookupCache/DifficultyRequest.cs
+++ BeatmapDifficultyLookupCache/DifficultyRequest.cs
@@ -2,14 +2,20 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using osu.Game.Online.API;
 
 namespace BeatmapDifficultyLookupCache
 {
     public class DifficultyRequest
     {
+        [JsonProperty("beatmap_id")]
         public int BeatmapId { get; set; }
+
+        [JsonProperty("ruleset_id")]
         public int RulesetId { get; set; }
+
+        [JsonProperty("mods")]
         public List<APIMod>? Mods { get; set; }
     }
 }
