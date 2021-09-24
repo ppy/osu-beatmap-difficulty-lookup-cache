0de5c0bcd7ec006b7049a2bd3950e97a86d37a6c
smoogipoo <smoogipoo@smgi.me>
2021-09-25 02:22:12 +0900

    Fix hash code and equality comparisons

--- BeatmapDifficultyLookupCache/DifficultyRequest.cs
+++ BeatmapDifficultyLookupCache/DifficultyRequest.cs
@@ -2,8 +2,11 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 using osu.Game.Online.API;
+using osu.Game.Utils;
 
 namespace BeatmapDifficultyLookupCache
 {
@@ -18,17 +21,39 @@ namespace BeatmapDifficultyLookupCache
         [JsonProperty("mods")]
         public APIMod[] Mods { get; init; } = Array.Empty<APIMod>();
 
+        private IEnumerable<APIMod> getOrderedMods => Mods.OrderBy(m => m.Acronym);
+
         public bool Equals(DifficultyRequest? other)
         {
             if (ReferenceEquals(null, other)) return false;
             if (ReferenceEquals(this, other)) return true;
 
-            return BeatmapId == other.BeatmapId && RulesetId == other.RulesetId && Equals(Mods, other.Mods);
+            return BeatmapId == other.BeatmapId && RulesetId == other.RulesetId && getOrderedMods.SequenceEqual(other.getOrderedMods);
         }
 
         public override bool Equals(object? obj)
             => obj is DifficultyRequest other && Equals(other);
 
-        public override int GetHashCode() => HashCode.Combine(BeatmapId, RulesetId, Mods);
+        public override int GetHashCode()
+        {
+            var hashCode = new HashCode();
+
+            hashCode.Add(BeatmapId);
+            hashCode.Add(RulesetId);
+
+            // Todo: Temporary (APIMod doesn't implement GetHashCode()).
+            foreach (var m in getOrderedMods)
+            {
+                hashCode.Add(m.Acronym);
+
+                foreach (var (key, value) in m.Settings)
+                {
+                    hashCode.Add(key);
+                    hashCode.Add(ModUtils.GetSettingUnderlyingValue(value));
+                }
+            }
+
+            return hashCode.ToHashCode();
+        }
     }
 }
